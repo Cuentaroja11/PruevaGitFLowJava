94a54969497c8c78530ec3ccbdd56b42ebe6fd64
Edwar Zacarias <cuentaroja11@gmail.com>
2020-11-23 14:07:21 -0600

    Se agrego hola mundo

--- Prueva/src/prueva/Prueva.java
+++ Prueva/src/prueva/Prueva.java
@@ -11,11 +11,14 @@ package prueva;
  */
 public class Prueva {
 
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hola Mundo");
         // TODO code application logic here
+        
     }
     
 }
